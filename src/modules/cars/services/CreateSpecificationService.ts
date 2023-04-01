9e3d94394cdf5138caba832032363578d86b55fa
Cristhoffer Santos <nunes-santos@hotmail.com>
2023-04-01 12:38:51 -0300

    feat: creating the specification repository

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,6 +1,24 @@
+import { ISpecificationRepository } from "../repositories/ISpecificationRepository"
+
+interface IRequest {
+  name: string
+  description: string
+}
 class CreateSpecificationService {
-  execute() {
-    //TODO
+  constructor(private specificationsRepository: ISpecificationRepository) {}
+
+  execute({ name, description }: IRequest): void {
+    const specificationAlreadyExists =
+      this.specificationsRepository.findByName(name)
+
+    if (specificationAlreadyExists) {
+      throw new Error("Specification  already exists")
+    }
+
+    this.specificationsRepository.create({
+      name,
+      description,
+    })
   }
 }
 
