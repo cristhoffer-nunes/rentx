4c5231a3f1ca94ee1994302115f22aa20f04104b
Cristhoffer Santos <nunes-santos@hotmail.com>
2023-04-01 22:36:19 -0300

    feat: creating the specification use case

--- src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -1,10 +1,10 @@
-import { ISpecificationRepository } from "../repositories/ISpecificationRepository"
+import { ISpecificationRepository } from "../../repositories/ISpecificationRepository"
 
 interface IRequest {
   name: string
   description: string
 }
-class CreateSpecificationService {
+class CreateSpecificationUseCase {
   constructor(private specificationsRepository: ISpecificationRepository) {}
 
   execute({ name, description }: IRequest): void {
@@ -22,4 +22,4 @@ class CreateSpecificationService {
   }
 }
 
-export { CreateSpecificationService }
+export { CreateSpecificationUseCase }
--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,21 +1,10 @@
-import { request, response, Router } from "express"
-import { SpecificationRepository } from "../modules/cars/repositories/implementations/SpecificationRepository"
-import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationService"
+import { Router } from "express"
+import { createSpecificationController } from "../modules/cars/useCases/createSpecification"
 
 const specificationsRoutes = Router()
 
-const specificationsRepository = new SpecificationRepository()
-
 specificationsRoutes.post("/", (request, response) => {
-  const { name, description } = request.body
-
-  const createSpecificationService = new CreateSpecificationService(
-    specificationsRepository
-  )
-
-  createSpecificationService.execute({ name, description })
-
-  return response.status(201).send()
+  return createSpecificationController.handle(request, response)
 })
 
 export { specificationsRoutes }
