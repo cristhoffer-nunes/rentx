1044a316d882720e98408b2be341675e0394bbcc
Cristhoffer Santos <nunes-santos@hotmail.com>
2023-04-05 00:56:15 -0300

    feat: creating the user controller

--- src/modules/accounts/useCases/createUser/CreateUserController.ts
+++ src/modules/accounts/useCases/createUser/CreateUserController.ts
@@ -1,3 +1,23 @@
-class CreateUserController {}
+import { Request, Response } from "express"
+import { container, injectable } from "tsyringe"
+import { CreateUserUseCase } from "./CreateUserUseCase"
+
+class CreateUserController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { name, username, email, password, driver_license } = request.body
+
+    const createUserUseCase = container.resolve(CreateUserUseCase)
+
+    await createUserUseCase.execute({
+      name,
+      username,
+      email,
+      password,
+      driver_license,
+    })
+
+    return response.status(201).send()
+  }
+}
 
 export { CreateUserController }
--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,7 +1,8 @@
-import { inject } from "tsyringe"
+import { inject, injectable } from "tsyringe"
 import { IUsersRepository } from "../../repositories/IUsersRepository"
 import { ICreateUserDTO } from "../../../dtos/ICreateUserDTO"
 
+@injectable()
 class CreateUserUseCase {
   constructor(
     @inject("UsersRepository")
