c708806485b72eb4bc001dbbb5a844b955044485
Cristhoffer Santos <nunes-santos@hotmail.com>
2023-04-05 12:57:43 -0300

    feat: encrypting the password

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from "tsyringe"
+import { hash } from "bcrypt"
 import { IUsersRepository } from "../../repositories/IUsersRepository"
 import { ICreateUserDTO } from "../../../dtos/ICreateUserDTO"
 
@@ -15,10 +16,18 @@ class CreateUserUseCase {
     password,
     driver_license,
   }: ICreateUserDTO): Promise<void> {
+    const userAlreadyExists = await this.usersRepository.findByEmail(email)
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists")
+    }
+
+    const passwordHash = await hash(password, 8)
+
     await this.usersRepository.create({
       name,
       email,
-      password,
+      password: passwordHash,
       driver_license,
     })
   }
