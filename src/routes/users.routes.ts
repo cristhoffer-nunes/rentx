a3fd56a960307632ecc7fec7a306b5e1bf7f9973
Cristhoffer Santos <nunes-santos@hotmail.com>
2023-04-06 13:17:51 -0300

    feat: uploading an avatar and removing the existing avatar file

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,23 @@
 import { Router } from "express"
 import { CreateUserController } from "../modules/accounts/useCases/createUser/CreateUserController"
+import { UpdateUserAvatarController } from "../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController"
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated"
+import uploadConfig from "../config/upload"
+import multer from "multer"
 
 const usersRoutes = Router()
 
+const uploadAvatar = multer(uploadConfig.upload("./tmp/avatar"))
+
 const createUserController = new CreateUserController()
+const updateUserAvatarContrller = new UpdateUserAvatarController()
 
 usersRoutes.post("/", createUserController.handle)
+usersRoutes.patch(
+  "/avatar",
+  ensureAuthenticated,
+  uploadAvatar.single("avatar"),
+  updateUserAvatarContrller.handle
+)
 
 export { usersRoutes }
